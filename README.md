a6833ab360e2f679d3126deacda915fd93351b96
Ivan Ermilov <earthquakesan@gmail.com>
2016-06-09 12:37:25 +0200

    URLs are in big-data-europe domain now

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # docker-demo-integrator-ui
-Showcase the demo for integrator UI with Hadoop, HDFS browser, Spark, Flink, Strabon, Sextant, Solr.
+Showcase the demo for integrator UI with Hadoop, HDFS browser, Spark, Flink, Strabon, Sextant, and Solr.
---
+++ scripts/inject-etc-hosts.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+HOSTS="hadoop.sc1.big-data-europe.eu hdfsfb.sc1.big-data-europe.eu spark.sc1.big-data-europe.eu solr.sc1.big-data-europe.eu flink.sc1.big-data-europe.eu strabon.sc1.big-data-europe.eu sextant.sc1.big-data-europe.eu"
+
+for host in $HOSTS; do
+  echo "127.0.0.1 $host" | sudo tee -a /etc/hosts; 
+done
+
