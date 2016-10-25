edaee194647dd068518f6213663eaef881cbed36
Ivan Ermilov <earthquakesan@gmail.com>
2016-10-25 11:55:58 +0200

    script to inject domain names into /etc/hosts for local testing

---
+++ edit-hosts.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "127.0.0.1 namenode.big-data-europe.local" >> /etc/hosts
+echo "127.0.0.1 resourcemanager.big-data-europe.local" >> /etc/hosts
+echo "127.0.0.1 historyserver.big-data-europe.local" >> /etc/hosts
+echo "127.0.0.1 nodemanager.big-data-europe.local" >> /etc/hosts
+echo "127.0.0.1 datanode.big-data-europe.local" >> /etc/hosts
+echo "127.0.0.1 hdfsfb.big-data-europe.local" >> /etc/hosts
+echo "127.0.0.1 sparkmaster.big-data-europe.local" >> /etc/hosts
+echo "127.0.0.1 sparkworker.big-data-europe.local" >> /etc/hosts
+echo "127.0.0.1 flinkmaster.big-data-europe.local" >> /etc/hosts
+echo "127.0.0.1 flinkworker.big-data-europe.local" >> /etc/hosts
+echo "127.0.0.1 solr.big-data-europe.local" >> /etc/hosts
+echo "127.0.0.1 strabon.big-data-europe.local" >> /etc/hosts
+echo "127.0.0.1 sextant.big-data-europe.local" >> /etc/hosts
