9403d34f1c641756ae5486049c402288fbbcb944
Ivan Ermilov <earthquakesan@gmail.com>
2016-10-26 12:46:34 +0200

    ready to test for csswrapper

--- edit-hosts.sh
+++ edit-hosts.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-echo "127.0.0.1 namenode.big-data-europe.local" >> /etc/hosts
-echo "127.0.0.1 resourcemanager.big-data-europe.local" >> /etc/hosts
-echo "127.0.0.1 historyserver.big-data-europe.local" >> /etc/hosts
-echo "127.0.0.1 nodemanager.big-data-europe.local" >> /etc/hosts
-echo "127.0.0.1 datanode.big-data-europe.local" >> /etc/hosts
-echo "127.0.0.1 hdfsfb.big-data-europe.local" >> /etc/hosts
-echo "127.0.0.1 sparkmaster.big-data-europe.local" >> /etc/hosts
-echo "127.0.0.1 sparkworker.big-data-europe.local" >> /etc/hosts
-echo "127.0.0.1 flinkmaster.big-data-europe.local" >> /etc/hosts
-echo "127.0.0.1 flinkworker.big-data-europe.local" >> /etc/hosts
-echo "127.0.0.1 solr.big-data-europe.local" >> /etc/hosts
-echo "127.0.0.1 strabon.big-data-europe.local" >> /etc/hosts
-echo "127.0.0.1 sextant.big-data-europe.local" >> /etc/hosts
+echo "127.0.0.1 namenode.big-data-europe.aksw.org" >> /etc/hosts
+echo "127.0.0.1 datanode.big-data-europe.aksw.org" >> /etc/hosts
+echo "127.0.0.1 hadoop-resource-manager.big-data-europe.aksw.org" >> /etc/hosts
+echo "127.0.0.1 hadoop-history-server.big-data-europe.aksw.org" >> /etc/hosts
+echo "127.0.0.1 hadoop-node-manager.big-data-europe.aksw.org" >> /etc/hosts
+echo "127.0.0.1 hue.big-data-europe.aksw.org" >> /etc/hosts
+echo "127.0.0.1 spark-master.big-data-europe.aksw.org" >> /etc/hosts
+echo "127.0.0.1 spark-worker.big-data-europe.aksw.org" >> /etc/hosts
+echo "127.0.0.1 flink-master.big-data-europe.aksw.org" >> /etc/hosts
+echo "127.0.0.1 flink-worker.big-data-europe.aksw.org" >> /etc/hosts
+echo "127.0.0.1 solr.big-data-europe.aksw.org" >> /etc/hosts
+echo "127.0.0.1 strabon.big-data-europe.aksw.org" >> /etc/hosts
+echo "127.0.0.1 sextant.big-data-europe.aksw.org" >> /etc/hosts
